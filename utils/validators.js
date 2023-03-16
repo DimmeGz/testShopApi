88ff4f291fccfd2fa3a1c684033a8634d744291f
Dima Serhiienko <dima.sergienko@gmail.com>
2023-03-16 14:00:22 +0200

    Update orderValidators: add check userId and productId by regex

--- utils/validators.js
+++ utils/validators.js
@@ -18,19 +18,25 @@ export const productValidators = [
 export const orderValidators = [
     body('user')
         .custom(async value => {
-            const user = await User.findById(value)
-            if (!user) {
-                throw new Error('User does not exist')
+            if (value.match(/^[0-9a-fA-F]{24}$/)) {
+                const user = await User.findById(value)
+                if (!user) {
+                    throw new Error('User does not exist')
+                }
+                return true
             }
-            return true
+            throw new Error('User does not exist')
         }),
     body('product')
         .custom(async value => {
-            const product = await Product.findById(value)
-            if (!product) {
-                throw new Error('Product does not exist')
+            if (value.match(/^[0-9a-fA-F]{24}$/)) {
+                const product = await Product.findById(value)
+                if (!product) {
+                    throw new Error('Product does not exist')
+                }
+                return true
             }
-            return true
+            throw new Error('Product does not exist')
         }),
     body('qty').isInt({min: 1, max: 999}),
 ]
