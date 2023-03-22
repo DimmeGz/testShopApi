a0aca1285b85a430b5827afcdc2af5ddada8e758
Dima Serhiienko <dima.sergienko@gmail.com>
2023-03-22 14:43:48 +0200

    Add pagination for orders and products

--- product/product.routes.ts
+++ product/product.routes.ts
@@ -2,13 +2,21 @@ import express, {Router} from 'express'
 
 import {Product} from './product.schema'
 import passport from "../middleware/passport";
+import {getPaginationParameters} from "../utils/functions"
 
 export const router = Router()
 
 router.get('/', async (req, res) => {
+    const {page, elementsCount, skipIndex} = getPaginationParameters(req)
+
+    const total_pages = Math.ceil(await Product.count() / elementsCount)
+
     try {
-        const products = await Product.find()
-        res.json(products)
+        const data = await Product.find()
+            .sort({ _id: 1 })
+            .limit(elementsCount)
+            .skip(skipIndex)
+        res.json({page, total_pages, elementsCount, data})
     } catch (e) {
         res.status(500).json({message: 'something went wrong'})
     }
