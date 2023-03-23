861b3b9a9a65b8ac3d8dd843749a62dfa6c93c85
Dima Serhiienko <dima.sergienko@gmail.com>
2023-03-23 17:48:59 +0200

    Update code in checkUser function: add types, delete ts-ignore

--- middleware/checkUser.ts
+++ middleware/checkUser.ts
@@ -4,30 +4,23 @@ import {Op} from 'sequelize'
 import {User} from '../user/user.model'
 
 export async function checkUser(req: Request, res: Response, next: any){
-    const token = req.headers['authorization']?.split(' ')[1]
-
-
     if (req.headers && req.headers.authorization) {
         const authorization = req.headers.authorization.split(' ')[1]
         try {
-            const decoded = jwt.verify(authorization, process.env.JWT_SECRET!);
-            // @ts-ignore
+            req.userRole = 'user'
+            const decoded: any = jwt.verify(authorization, process.env.JWT_SECRET!)
             const authField = (decoded.user[Object.keys(decoded.user)[0]])
-            const user = await User.findOne({ where: { [Op.or]: [{ email: authField }, { phone: authField }]}})
-            // @ts-ignore
+            const user: any = await User.findOne({ where: { [Op.or]: [{ email: authField }, { phone: authField }]}})
             if (user.role === 'admin') {
                 req.userRole = 'admin'
                 next()
             } else {
-                req.userRole = 'user'
                 next()
             }
         } catch (e) {
-            req.userRole = 'user'
             next()
         }
     } else {
-        req.userRole = 'user'
         next()
     }
 }
